5908d4e334ed513e5448fe4826de897133dffb02
furiax <carlmalfliet@proximus.be>
2023-10-09 20:20:50 +0200

    added movie model scaffoled controller added globalize so i can enter decimal at price

---
+++ Movies.Furiax/Movies/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movies.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        [Display(Name ="Release Date"), DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
--- Movies.Furiax/Movies/Program.cs
+++ Movies.Furiax/Movies/Program.cs
@@ -1,4 +1,12 @@
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Movies.Data;
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MoviesContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MoviesContext") ?? throw new InvalidOperationException("Connection string 'MoviesContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -12,6 +20,15 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+var defaultCulture = new CultureInfo("nl-BE");
+var localizationOptions = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture(defaultCulture),
+    SupportedCultures = new List<CultureInfo> { defaultCulture },
+    SupportedUICultures = new List<CultureInfo> { defaultCulture }
+};
+app.UseRequestLocalization(localizationOptions);
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
