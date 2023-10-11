6305ae040e80377b03b3645f29e1203f49e9f6ff
furiax <carlmalfliet@proximus.be>
2023-10-11 21:57:07 +0200

    added extra validation

--- Movies.Furiax/Movies/Models/Music.cs
+++ Movies.Furiax/Movies/Models/Music.cs
@@ -6,13 +6,15 @@ namespace Movies.Models
     public class Music
     {
         public int Id { get; set; }
+        [StringLength(60, MinimumLength = 3), Required]
         public string? Artist { get; set; }
-        [Display(Name ="Album Name")]
+        [StringLength(60, MinimumLength = 3), Display(Name ="Album Name"), Required]
         public string? AlbumName { get; set; }
         [Display(Name = "Release Date"), DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"),Required, StringLength(30)]   
         public string? Genre { get; set; }
-        [Column(TypeName = "decimal(18,2)"), DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,2)"), DataType(DataType.Currency), Range(1, 50), Required]
         public decimal Price { get; set; }
     }
 }
