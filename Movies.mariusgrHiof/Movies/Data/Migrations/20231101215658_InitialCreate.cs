44b2b0a5589b872d9b03e5e4d15e7333c2045b3b
Marius Gravningsmyhr <marius.gravningsmyhr@gmail.com>
2023-11-04 20:55:24 +0100

    Initial commit

---
+++ Movies.mariusgrHiof/Movies/Data/Migrations/20231101215658_InitialCreate.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Movies.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    Genre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DurationInMinutes = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "DurationInMinutes", "Genre", "Title", "Year" },
+                values: new object[,]
+                {
+                    { 1, 90, "Drama", "Rocky 4", 1985 },
+                    { 2, 77, "Drama", "Rocky 3", 1983 },
+                    { 3, 95, "Drama", "Rocky 2", 1979 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
---
+++ Movies.mariusgrHiof/Movies/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace Movies.Models;
+public class Movie
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public int Year { get; set; }
+    public string Genre { get; set; } = string.Empty;
+    public int DurationInMinutes { get; set; }
+
+}
+
