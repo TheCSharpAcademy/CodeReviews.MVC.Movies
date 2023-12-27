2cbba9e01284d27e347e652e7042fab4ee6f618f
simon davies <daviessimon21@gmail.com>
2023-12-27 22:29:43 +0000

    Complete

---
+++ MVC.Movies.Speedierone/MVC.Movies.Speedierone/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC.Movies.Speedierone.Models
+{
+    public class Movie
+    {
+        public int Id {  get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public decimal Price { get; set; }
+        public string Rating { get; set; }
+    }
+}
