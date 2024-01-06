75905a0d6f48d0c748813b98021b7344021e2167
K-MYR <144546255+K-MYR@users.noreply.github.com>
2024-01-06 08:26:25 +0000

    Initial Setup

---
+++ MVC.Movies.K-MYR/MVC.Movies.K-MYR/Controllers/MoviesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Movies.K_MYR;
+
+public class MoviesController : Controller
+{
+    private readonly ILogger<MoviesController> _logger;
+
+    public MoviesController(ILogger<MoviesController> logger)
+    {
+        _logger = logger;        
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ MVC.Movies.K-MYR/MVC.Movies.K-MYR/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC.Movies.K_MYR;
+
+public class Movie
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    [DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
+    public string? Genre { get; set; }
+    public decimal Price { get; set; }
+}
