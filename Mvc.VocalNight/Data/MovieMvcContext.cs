671662856bff84c702f4f58df01f7fd47167bdc3
Vocal <vocalnight@protonmail.com>
2024-01-10 22:43:10 -0300

    project finished

---
+++ Mvc.VocalNight/Data/MovieMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieMvc.Models;
+
+namespace MovieMvc.Data
+{
+    public class MovieMvcContext : DbContext
+    {
+        public MovieMvcContext (DbContextOptions<MovieMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieMvc.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ Mvc.VocalNight/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MovieMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
