666e5d6fab6c6b30a4940fe6cb667ff692f87e5d
Stevie Robinson <stevie.robinson@gmail.com>
2024-01-30 22:30:52 +0100

    Complete MVC Movie Tutorial

---
+++ Movies.StevieTV/Movies.StevieTV/Models/MovieGenreViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Movies.StevieTV.Models;
+
+public class MovieGenreViewModel
+{
+    public List<Movie>? Movies { get; set; }
+    public SelectList? Genres { get; set; }
+    public string? MovieGenre { get; set; }
+    public string? SearchString { get; set; }
+}
