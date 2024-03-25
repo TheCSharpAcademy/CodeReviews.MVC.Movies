98412cedaaeec3d34fc62af1ad7b400c494b9be2
ManuelE-Osorio <aux.manuel.osorio@gmail.com>
2024-03-25 15:50:27 -0500

    add README.md and refactor JavaScript

--- Movies.ManuelE-Osorio/Movies/wwwroot/js/site.js
+++ Movies.ManuelE-Osorio/Movies/wwwroot/js/site.js
@@ -21,24 +21,24 @@ function themeSwitch() {
     }
 }
 
-function setCookie(cname, cvalue, exdays) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    let expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+function setCookie(cookieName, cookieValue, expirationDays) {
+    const cookieDate = new Date();
+    cookieDate.setTime(cookieDate.getTime() + (expirationDays*24*60*60*1000));
+    let expires = "expires="+ cookieDate.toUTCString();
+    document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/;SameSite=Strict";
 }
 
-function getCookie(cname) {
-    let name = cname + "=";
+function getCookie(cookieName) {
+    let name = cookieName + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
+    let cookieArray = decodedCookie.split(';');
+    for(let i = 0; i <cookieArray.length; i++) {
+      let cookie = cookieArray[i];
+      while (cookie.charAt(0) == ' ') {
+        cookie = cookie.substring(1);
       }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
+      if (cookie.indexOf(name) == 0) {
+        return cookie.substring(name.length, c.length);
       }
     }
     return "";
