7704ff9d0d5b4899af3b6802a1ce262c949463e8
frockett <crockett.d.ford@gmail.com>
2024-04-23 21:04:42 +0800

    implemented model validation

--- Movies.frockett/Movies.frockett/Models/Movie.cs
+++ Movies.frockett/Movies.frockett/Models/Movie.cs
@@ -7,12 +7,27 @@ namespace Movies.frockett.Models;
 public class Movie
 {
     public int Id { get; set; }
+
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
     public string? Title { get; set; }
+
     [DataType(DataType.Date)]
     [Display(Name = "Release Date")]
     public DateTime ReleaseDate { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+    [Required]
+    [StringLength(30)]
     public string? Genre { get; set; }
+
+    [Range(1, 100)]
+    [DataType(DataType.Currency)]
     [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+    [StringLength(5)]
+    //[Required]
     public string? Rating { get; set; }
 }
