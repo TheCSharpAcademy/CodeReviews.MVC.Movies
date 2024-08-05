9ab210fe4dbfe9a5d4a116fc43cdd6be558534be
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2024-08-05 15:09:54 -0400

    part way through tutorial

---
+++ Movies.kwm0304/Controllers/MovieController.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+using Movies.kwm0304.Models;
+
+namespace Movies.kwm0304.Controllers;
+
+public class MovieController : Controller
+{
+  public IActionResult Index()
+  {
+    return View();
+  }
+
+  public string Welcome(string name, int numTimes = 1)
+  {
+    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+  }
+
+}
